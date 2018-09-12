49599579eec4a95f6b877b2e3f74227759abf2f9
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-09-13 00:44:56 +0300

    Initial tests structure.

---
+++ tests/DarkSky/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Illuminated\DarkSky\Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_will_be_done()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/DarkSky/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Illuminated\DarkSky\Tests;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+}
