f4499ce575c5b07eb3030cf21191a6b8ec13b6c9
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-09-13 21:51:25 +0300

    Request url logic added - for forecast and time machine requests both.

--- src/DarkSky.php
+++ src/DarkSky.php
@@ -12,4 +12,15 @@ class DarkSky
         $this->latitude = (float) $latitude;
         $this->longitude = (float) $longitude;
     }
+
+    protected function url($time = null)
+    {
+        $key = config('dark-api.key');
+
+        $latitude = $this->latitude;
+        $longitude = $this->longitude;
+        $uri = collect([$latitude, $longitude, $time])->filter()->implode(',');
+
+        return "https://api.darksky.net/forecast/{$key}/{$uri}";
+    }
 }
