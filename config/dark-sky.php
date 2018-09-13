028b4c5f9b6757c5ac4dfb45a6bc4273759299b3
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-09-13 16:11:44 +0300

    Config continued - extend option added.

--- config/dark-sky.php
+++ config/dark-sky.php
@@ -15,4 +15,19 @@ return [
 
     'key' => env('DARK_SKY_KEY', null),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Extend Forecast
+    |--------------------------------------------------------------------------
+    |
+    | Use this option to extend the forecast request with an additional data.
+    | For example, hour-by-hour data can be extended from 48 to 168 hours.
+    | Note that this option has no effect on the time machine requests.
+    |
+    | Supported: "hourly", null.
+    |
+    */
+
+    'extend' => null,
+
 ];
